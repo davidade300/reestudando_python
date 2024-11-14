3c88150fa1a90b945ec2e0a03dfcc92ca55d1c99
davidade300 <iaderinho@icloud.com>
2024-11-14 11:54:41 -0300

    métodos em instancias de classes

---
+++ py_estudo/oop/carros.py
@@ -0,0 +1,20 @@
+# Métodos em instancias de classes
+# Hard coded -> algo que foi escrito diretamente no código
+
+
+class Carro:
+    def __init__(self, nome) -> None:
+        # self.nome = "Fusca" -> Hard coded
+        self.nome = nome
+
+    def acelerar(self):  # para usar os dados da instância, basta passar o self
+        return "f{self.nome} está acelerando"
+
+
+fusca = Carro("fusca")
+print(fusca.nome)
+fusca.acelerar()
+
+
+celta = Carro("celta")
+print(celta.nome)
--- py_estudo/oop/classes_init.py
+++ py_estudo/oop/classes_init.py
@@ -1,6 +1,9 @@
 class Pessoa:
+    # caso parametros de classe sejam definidos fora do __init__,
+    # eles seram estáticos para todas as classes
     # o self é requerido em todos os inicializadores
-    def __init__(self, nome, sobrenome):
+    # o método init sempre retorna None
+    def __init__(self, nome, sobrenome) -> None:
         self.nome = nome
         self.sobrenome = sobrenome
 
