df73cb49a0486d1f9f851de4b2a41b0820e8a156
davidade300 <iaderinho@icloud.com>
2024-11-18 14:41:06 -0300

    notas e código sobre __dict__ e vars() adicionadas

--- py_estudo/oop/pessoa.py
+++ py_estudo/oop/pessoa.py
@@ -1,7 +1,4 @@
 # Atributos de classe
-# ANO_ATUAL
-
-
 # atributos primeiro são buscados na instancia da classe e posteriormente na classe em sí
 
 
@@ -30,3 +27,33 @@ Pessoa.ano_atual = 0
 
 print(p1.get_ano_nascimento())
 print(p2.get_ano_nascimento())
+
+
+# representação em dicionario da classe
+print(p1.__dict__)
+print(vars(p1))
+
+
+# é possível adicionar KV's que não existem na declaração da classe
+p1.__dict__["outra"] = "coisa"
+p1.__dict__["outra"] = "coisa"
+
+print(p1.__dict__)
+
+
+# o código abaixo gera erro, pois o dict dados possui uma chave que não corresponde
+# a um parametro da classe
+"""
+dados = {"idade": 45, "outra": "coisa"}
+p3 = Pessoa(**dados)  # desempacotamento de dicionários
+print(p3.__dict__)
+"""
+
+dados = {"nome": "Maria", "idade": 44}
+p3 = Pessoa(**dados)
+print(p3.__dict__)
+
+
+# Redefinindo P1
+p1 = Pessoa("João_2", 40)
+print(vars(p1))
